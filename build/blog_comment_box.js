b94a7c3893f272f334911884f53a30e855d0fce4
wellwind <wellwind@mail2000.com.tw>
2016-03-03 21:49:39 +0800

    add comment list & comment form into comment box

--- build/blog_comment_box.js
+++ build/blog_comment_box.js
@@ -5,8 +5,39 @@ var CommentBox = React.createClass({
     return React.createElement(
       "div",
       { className: "commentBox" },
-      "Hello, world! I am a CommentBox."
+      React.createElement(
+        "h1",
+        null,
+        "Comments"
+      ),
+      React.createElement(CommentList, null),
+      React.createElement(CommentForm, null)
     );
   }
 });
+
+var CommentList = React.createClass({
+  displayName: "CommentList",
+
+  render: function () {
+    return React.createElement(
+      "div",
+      { className: "commentList" },
+      "Hello, world! I am a CommentList."
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  displayName: "CommentForm",
+
+  render: function () {
+    return React.createElement(
+      "div",
+      { className: "commentForm" },
+      "Hello, world! I am a CommentForm."
+    );
+  }
+});
+
 ReactDOM.render(React.createElement(CommentBox, null), document.getElementById('content'));
