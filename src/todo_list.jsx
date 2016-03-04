fa9d51a0115386686fe8fa79b9a498b0d0ee7bf3
wellwind <wellwind@mail2000.com.tw>
2016-03-04 14:46:38 +0800

    add todo list form

--- src/todo_list.jsx
+++ src/todo_list.jsx
@@ -1,13 +1,6 @@
 var TodoItems = React.createClass({
-  getInitialState: function(){
-    var items = [
-      { id: 1, text: "Learn React."},
-      { id: 2, text: "Build todo list practice."}
-    ];
-    return {data: items};
-  },
   render: function() {
-    var items = this.state.data.map(function(item) {
+    var items = this.props.items.map(function(item) {
       return (
         <li key={item.id}>
           {item.text}
@@ -22,11 +15,59 @@ var TodoItems = React.createClass({
   }
 });
 
+var AddTodoForm = React.createClass({
+  getInitialState: function(){
+    return {todoText: ""};
+  },
+  handleAddTodo: function(e){
+    if(this.state.todoText !== ""){
+      this.props.addItem(this.state.todoText);
+      this.setState({todoText: ""});
+    }
+  },
+  handleAddTodoTyping: function(e){
+    var name = e.target.name
+    var value = e.target.value;
+    var obj={};
+    obj[name] = value;
+    this.setState(obj);
+  },
+  render: function(){
+    return (
+      <div classNam="addTodoForm">
+        <div>
+          Todo item:
+          <input type="text" name="todoText" value={this.state.todoText}
+            onChange={this.handleAddTodoTyping}></input>
+          <button
+            onClick={this.handleAddTodo}
+            disabled={this.state.todoText === "" ? "disabled" : ""}>Add</button>
+        </div>
+      </div>
+    );
+  }
+});
+
 var TodoList = React.createClass({
+  getInitialState: function(){
+    var items = [
+      { id: 1, text: "Learn React."},
+      { id: 2, text: "Build todo list practice."}
+    ];
+    return {items: items};
+  },
+  addTodoItem: function(item){
+    this.state.items.push({
+      id: new Date(),
+      text: item
+    });
+    this.setState({items: this.state.items});
+  },
   render: function(){
     return(
       <div className="todoList">
-        <TodoItems />
+        <TodoItems items={this.state.items}/>
+        <AddTodoForm addItem={this.addTodoItem}/>
       </div>
     );
   }
